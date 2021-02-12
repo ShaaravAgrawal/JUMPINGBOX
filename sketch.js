44377e5e15e59e9c653631c6a0861890953a1b2e
TheDemonSlayer <76558834+ShaaravAgrawal@users.noreply.github.com>
2021-02-12 17:07:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var canvas;
+var block1,block2,block3,block4;
+var ball, edges;
+var sound;
+
+function preload(){
+    sound = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    block1 = createSprite(0,580,360,30);
+    block1.shapeColor = "blue";
+
+    block2 = createSprite(295,580,200,30);
+    block2.shapeColor = "orange";
+
+    block3 = createSprite(515,580,200,30);
+    block3.shapeColor = "red";
+
+    block4 = createSprite(740,580,220,30);
+    block4.shapeColor = "green";
+
+    ball = createSprite(random(20,750),100, 40,40);
+    ball.shapeColor = "white";
+    ball.velocityX = 4;
+    ball.velocityY = 9;
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    ball.bounceOff(edges);
+
+    if(block1.isTouching(ball) && ball.bounceOff(block1)){
+        ball.shapeColor = "blue";
+        sound.play();
+    }
+
+    if(block2.isTouching(ball)){
+        ball.shapeColor = "orange";
+        ball.velocityX = 0;
+        ball.velocityY = 0;
+        sound.stop();
+    }
+
+    if(block3.isTouching(ball) && ball.bounceOff(block3)){
+        ball.shapeColor = "red";
+    }
+
+    if(block4.isTouching(ball) && ball.bounceOff(block4)){
+        ball.shapeColor = "green";
+    }
+
+    drawSprites();
+}
